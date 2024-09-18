349dac49cc689511074aebaebfd04907ac9ca954
Amir Chelly <amirchelly99@gmail.com>
2024-09-18 15:14:53 +0300

    refactor first iteration

--- src/main/kotlin/basics/Classes.kt
+++
@@ -1,9 +0,0 @@
-package topics
-
-fun main() {
-
-}
-
-class Rectangle(height: Int, length: Int) {
-    constructor(height: Int) : this(height, length = 0)
-}
--- src/main/kotlin/basics/Lambda.kt
+++
@@ -1,8 +0,0 @@
-package topics
-
-fun main() {
-    val numbers = listOf(1,1,3,2,5)
-    numbers.forEach{ e -> println(e) }
-    println(numbers.map { e -> e * 2 })
-    println(numbers.filter { e -> e == 1 })
-}
